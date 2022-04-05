67cabe6773a00460e8a93e51f39ccb2a240ee4a0
audevd <avrignaud74@gmail.com>
2022-04-05 08:55:54 +0200

    méthodes de balayage

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ if __name__ == '__main__':
     nom = input("Quel est ton nom ? ")
     nom = nom.title()
     print_hi(nom)
+
+#méthode par balayage à pas constant
+
+
+#méthode par balayage aléatoire
