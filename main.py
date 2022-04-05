360d1d4e916f2fbe45e8e9118fcaa80446f81fbe
audevd <avrignaud74@gmail.com>
2022-04-05 09:13:15 +0200

    méthode de balayage à pas constant

--- main.py
+++ main.py
@@ -2,7 +2,20 @@
 def print_hi(name):
     print("Salut", name, "!")
 
+def f(x):
+    return x**3-3*x**2+2*x+5
+
 # méthode par balayage à pas constant
+def balayage_const(a, b, N, f):
+    liste=[]
+
+    x=(a-b)/N
+    for i in range (1, N+1):
+        liste.append(f(i))
+        print(f(i))
+    r=min(liste)
+    print(a, ',', b, ',', N, ',', x, ',', liste, ',', r)
+    return x
 
 # méthode par balayage aléatoire
 
@@ -11,3 +24,4 @@ if __name__ == '__main__':
     nom = input("Quel est ton nom ? ")
     nom = nom.title()
     print_hi(nom)
+    balayage_const(1,3,6,f)
