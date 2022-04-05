e936a134fdc3dacaf0bf368aa4b7a960c15ca522
Romain <romain@cognasse.com>
2022-04-05 08:58:10 +0200

    on range un peu

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 def print_hi(name):
     print("Salut", name, "!")
 
+# méthode par balayage à pas constant
+
+# méthode par balayage aléatoire
+
+
 if __name__ == '__main__':
     nom = input("Quel est ton nom ? ")
     nom = nom.title()
     print_hi(nom)
-
-#méthode par balayage à pas constant
-
-
-#méthode par balayage aléatoire
